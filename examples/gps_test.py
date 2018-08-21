5a718c95a1594b7b3f79e7e02d42108311b5dd56
Wizehunt <kriszt4@gmail.com>
2018-08-22 01:47:04 +0200

    polished GPS helpers and made a GPS example

--- examples/gps_test.py
+++ examples/gps_test.py
@@ -1,50 +1,58 @@
 from elevages_numeriques.gps import *
 import time
 
-gps = Gps()
+gps = Gps() # declare your GPS  object
+
+# Choose which data you want to keep
+# By default, only datetime, latitude and longitude are kept
+# You can enable a field with gps.set_logging(FIELD_NAME, True)
+# and disable a field with gps.set_logging(FIELD_NAME, False)
+# where FIELD_NAME can be one of the following :
+# 'all' or '*'          : selects all the following fields at once
+# 'datetime',           : current date and time (UTC timezone !), formatted as 'YYYY-MM-DD hh:mm:ss'
+# 'latitude',           : latitude, in degrees
+# 'longitude',          : longitude, in degrees
+# 'altitude',           : altitude, in meters
+# 'speed',              : current speed, in Km/h
+# 'fix_quality',        : positionning quality indicator; 0=invalid, 1=valid, 2 and more: high precision
+# 'satellites',         : how many satellites do we see ?
+# 'horizontal_dilution' : GPS signal confidence indicator. The lower the better Check Wikipedia if you need more details.
+#                         <2:Excellent, 2-5:Good, 5-10:Moderate, 10 and more:Poor
+
+# example 1 : Everything except speed and altitude
+# gps.set_logging('all', True)
+# gps.set_logging('speed', False)
+# gps.set_logging('altitude', False)
+
+# example 2 : only speed, altitude and # of satellites, in that order
+# gps.set_logging('all', False)
+# gps.set_logging('speed', True)
+# gps.set_logging('altitude', True)
+# gps.set_logging('satellites', True)
+
+# example 3 (these are the fields by default) : datetime, latitude and longitude, in that order
+gps.set_logging('all', False)
+gps.set_logging('datetime', True)
+gps.set_logging('latitude', True)
+gps.set_logging('longitude', True)
+
 
 last_print = time.monotonic()
 while True:
-    # Make sure to call gps.update() every loop iteration and at least twice
-    # as fast as data comes from the GPS unit (usually every second).
-    # This returns a bool that's true if it parsed new data (you can ignore it
-    # though if you don't care and instead look at the has_fix property).
+    # Make sure to call gps.update() every loop iteration
+    # This returns a bool that's true if it got new data
     gps.update()
+    
     # Every second print out current location details if there's a fix.
-    current = time.monotonic()
-    if current - last_print >= 1.0:
-        last_print = current
-        if not gps.has_fix:
-            # Try again if we don't have a fix yet.
-            print('Waiting for fix...')
+    current_time = time.monotonic()
+    if current_time - last_print >= 1.0:
+        last_print = current_time
+        
+        if not gps.has_fix: # if the GPS has a fix, it means it knows where it is
+            print('Waiting for fix... {} satellite(s) found'.format(gps.satellites))
             continue
+        
         # We have a fix! (gps.has_fix is true)
         # Print out details about the fix like location, date, etc.
-        print('=' * 40)  # Print a separator line.
-        print('Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}'.format(
-            gps.timestamp_utc.tm_mon,   # Grab parts of the time from the
-            gps.timestamp_utc.tm_mday,  # struct_time object that holds
-            gps.timestamp_utc.tm_year,  # the fix time.  Note you might
-            gps.timestamp_utc.tm_hour,  # not get all data like year, day,
-            gps.timestamp_utc.tm_min,   # month!
-            gps.timestamp_utc.tm_sec))
-        print('Latitude: {0:.6f} degrees'.format(gps.latitude))
-        print('Longitude: {0:.6f} degrees'.format(gps.longitude))
-        print('Fix quality: {}'.format(gps.fix_quality))
-        # Some attributes beyond latitude, longitude and timestamp are optional
-        # and might not be present.  Check if they're None before trying to use!
-        if gps.satellites is not None:
-            print('# satellites: {}'.format(gps.satellites))
-        if gps.altitude_m is not None:
-            print('Altitude: {} meters'.format(gps.altitude_m))
-        if gps.track_angle_deg is not None:
-            print('Speed: {} knots'.format(gps.speed_knots))
-        if gps.track_angle_deg is not None:
-            print('Track angle: {} degrees'.format(gps.track_angle_deg))
-        if gps.horizontal_dilution is not None:
-            print('Horizontal dilution: {}'.format(gps.horizontal_dilution))
-        if gps.height_geoid is not None:
-            print('Height geo ID: {} meters'.format(gps.height_geoid))
-        
-        print(gps.header)
-        print(gps)
+        print(gps.header)   # this prints the enabled fields names, CSV formatted
+        print(gps)          # this prints the enabled fields content, CSV formatted
--- lib/elevages_numeriques/gps.py
+++ lib/elevages_numeriques/gps.py
@@ -4,21 +4,41 @@ import adafruit_gps
 
 class Gps(adafruit_gps.GPS):
     
-    _header = 'datetime;latitude;longitude;altitude;fix_quality;# satellites;horizontal dilution'
-    
-    _KEYS = ('datetime', 'latitude', 'longitude', 'fix_quality', 'satellites', 'horizontal_dilution')
-    
-    _FIELDS = (_KEYS[0:3]) # display first 3 fields by default
+    _KEYS = ('datetime', 'latitude', 'longitude', 'altitude', 'speed', 'fix_quality', 'satellites', 'horizontal_dilution')
     
     @property
     def header(self):
-        return ';'.join(Gps._FIELDS)
+        return ';'.join(self._fields)
+        
+    def set_logging(self, field_name, enable):
+        """
+        Enables or disables the logging output for the given field
+        """
+        if field_name == '*' or field_name == 'all': # wildcard : enable or disable ALL fields
+            if enable:
+                self._fields = list()# .extend(Gps._KEYS[0:3])
+                self._fields.extend(Gps._KEYS[0:len(Gps._KEYS)])
+            else:
+                self._fields = list()
+            return None# all fields were added or deleted, stop here
+        
+        # One-by-one enabling or disabling field
+        if not enable and field_name in self._fields:
+            self._fields.remove(field_name)
+        elif enable and field_name not in self._fields:
+            if field_name not in Gps._KEYS:
+                raise RuntimeError('field named {} is unknown to the GPS module'.format(field_name))
+            else:
+                self._fields.append(field_name)
     
     def __init__(self):
         RX = board.RX
         TX = board.TX
         uart = busio.UART(TX, RX, baudrate=9600, timeout=3000)
         super(Gps, self).__init__(uart, debug=False)
+        self._fields = list() # log 'datetime', 'latitude' and 'longitude' fields by default
+        self._fields.extend(Gps._KEYS[0:3])
+        
         
     def timestamp_to_datetime(self):
         """
@@ -28,16 +48,49 @@ class Gps(adafruit_gps.GPS):
         time = self.timestamp_utc
         return '{}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}'.format(time.tm_year, time.tm_mon, time.tm_mday, time.tm_hour, time.tm_min, time.tm_sec)
         
+    def knots_to_kmh(self):
+        """
+        Converts provided GPS speed (in knots) to standard Km/h 1
+        """
+        speed_knt = self.speed_knots
+        speed_kmh = speed_knt / 1852 # 1 knot == 1852 meter/hour
+        return speed_kmh
+
+    
+
     def __str__(self):
+        content = list()
+        
+        for key in self._fields:
+            if key == 'datetime':
+                content.append(self.timestamp_to_datetime())
+            elif key == 'latitude':
+                content.append(self.latitude)
+            elif key == 'longitude':
+                content.append(self.longitude)
+            elif key == 'altitude':
+                content.append(self.altitude_m)
+            elif key == 'speed':
+                content.append(self.knots_to_kmh())
+            elif key == 'fix_quality':
+                content.append(self.fix_quality)
+            elif key == 'satellites':
+                content.append(self.satellites)
+            elif key == 'horizontal_dilution':
+                content.append(self.horizontal_dilution)
+            else:
+                raise RuntimeError('something went wrong fetching unknown field {}'.format(key))
+        
+        for i in range(0,  len(content)):
+            content[i] = str(content[i])
+
+        return ';'.join(content)
+        
+        """
         return '{};{};{};{};{};{};{}'.format(
             self.timestamp_to_datetime(), 
             self.latitude, 
             self.longitude, 
             self.altitude_m, 
-            self.fix_quality,
-            self.satellites, 
-            self.horizontal_dilution)
-    
-    
-    
-    
+            self.knots_to_kmh(),
+        """
