7c4e754573db1166d8a0e7da454a1858682af989
Wizehunt <kriszt4@gmail.com>
2018-10-13 20:39:11 +0200

    doc updated

--- docs/conf.py
+++ docs/conf.py
@@ -4,6 +4,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../lib'))
 sys.path.insert(0, os.path.abspath('../lib/elevages_numeriques'))
 
 # -- General configuration ------------------------------------------------
