e1a46cc98b89b024120fedc1c209dcbae90f961d
Wizehunt <kriszt4@gmail.com>
2018-10-13 18:41:50 +0200

    doc updated

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../lib'))
 
@@ -16,7 +17,12 @@ extensions = [
     'sphinx.ext.viewcode',
 ]
 
-intersphinx_mapping = {'python': ('https://docs.python.org/3.4', None),'CircuitPython': ('https://circuitpython.readthedocs.io/en/latest/', None)}
+autodoc_mock_imports = ["board"]
+
+intersphinx_mapping = {
+	'python': ('https://docs.python.org/3.4', None),
+	'CircuitPython': ('https://circuitpython.readthedocs.io/en/latest/', None)
+	}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
