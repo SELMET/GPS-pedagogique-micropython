0bf4590571016e6f0597eb672f2ce554e221cfe4
Wizehunt <kriszt4@gmail.com>
2018-10-16 01:15:41 +0200

    doc updated

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ extensions = [
     'sphinx.ext.viewcode',
 ]
 
-autodoc_mock_imports = ["board", "adafruit_dotstar"]
+autodoc_mock_imports = ["board", "adafruit_dotstar", "Pin"]
 
 intersphinx_mapping = {
 	'python': ('https://docs.python.org/3.4', None),
